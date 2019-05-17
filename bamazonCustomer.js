1dddc3aec1c542a05c906711a37983652e31e7b0
Amber Koval <amberkoval92@gmail.com>
2019-05-16 18:46:36 -0700

    prompt working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,11 +3,11 @@ var inquirer = require("inquirer");
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
 });
 // *************Connects and displays items*************
 // connection.connect(function(err) {
@@ -24,14 +24,49 @@ var connection = mysql.createConnection({
 // *************Connects and displays items*************
 
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    showInventory();
-  });
+    showInventory(); //start
+});
 
-  function showInventory() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
+
+function showInventory() {
+
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([
+                {
+                    name: "storeGoods",
+                    type: "rawlist",
+                    choices: function () {
+
+                        var pushItems = [];
+
+                        for (var i = 0; i < results.length; i++) {
+                            pushItems.push(results[i].product_name);
+                        }
+                        return pushItems;
+                    },
+                    message: "What is the id of the item you would like to purchase?"
+                },
+                {
+                    name: "buy",
+                    type: "input",
+                    message: "How many would you like to buy?"
+                }
+            ])
+            .then(function (answer) {
+                var selectedItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].product_name === answer.choice) {
+                        selectedItem = results[i];
+                    }
+                }
+
+            }).catch(function (error) {
+                console.log(error)
+            });
     });
-  }
+}
